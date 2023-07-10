1fadbdf3a541114614ca77efb237afdd69092a2e
develoHAM <kdw980722@gmail.com>
2023-07-10 20:00:11 +0900

    JavaScript tutorial 3 & 4

---
+++ JavaScript/2023_07_10_js_tutorial_3.js
@@ -0,0 +1,2 @@
+console.log(`${typeof 5} isn't ${typeof "5"} data type.`);
+console.log(`Typeof를 boolean이나 null에 사용하면, ${typeof false} 결과를 얻을 수 있습니다.`)
---
+++ JavaScript/2023_07_10_js_tutorial_4.js
@@ -0,0 +1,4 @@
+let mathScore = "77";
+let engScore = "88";
+let avg = (Number(mathScore) + Number(engScore)) / 2
+console.log(avg);
