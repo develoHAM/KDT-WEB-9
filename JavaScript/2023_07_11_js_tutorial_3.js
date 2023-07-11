8f5ebbe6418afb324d1fd1d293285a85e212a537
develoHAM <kdw980722@gmail.com>
2023-07-11 16:05:07 +0900

    JavaScript function & onlick tutorial 3

---
+++ JavaScript/2023_07_11_js_tutorial_3.js
@@ -0,0 +1,20 @@
+let number1 = prompt("fist number");
+        let number2 = prompt("second number");
+        console.log(`first number is: ${number1}`);
+        console.log(`second number is: ${number2}`);
+        function add( num1, num2) {
+            alert(Number(num1) + Number(num2));
+            console.log(Number(num1) + Number(num2));
+        }
+        function subtract( num1, num2) {
+            alert(Number(num1) - Number(num2));
+            console.log(Number(num1) - Number(num2));
+        }
+        function multiply( num1, num2) {
+            alert(Number(num1) * Number(num2));
+            console.log(Number(num1) * Number(num2));
+        }
+        function divide( num1, num2) {
+            alert(Number(num1) / Number(num2));
+            console.log(Number(num1) / Number(num2));
+        }
