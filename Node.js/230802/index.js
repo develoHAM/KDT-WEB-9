b1f1ad23d9ef41daf8ef615be460a7c80c98075e
develoHAM <kdw980722@gmail.com>
2023-08-02 20:03:19 +0900

    dynamic form practice

--- Node.js/230802/index.js
+++ Node.js/230802/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 8000;
 
+//practice 2 variables
+const id = "codingOn";
+const pw = "1234";
+
 // body parser
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -36,6 +40,43 @@ app.post('/axios', (req, res) => {
     console.log('back', req.body);
     res.send(req.body);
 });
+
+// fetch
+app.get('/fetch', (req, res) => {
+    console.log('back', req.query);
+    res.send(req.query);
+})
+
+app.post('/fetch', (req, res) => {
+    console.log('back', req.body);
+    res.send(req.body);
+})
+
+
+
+// practice1
+app.get('/practice1', (req, res) => {
+    res.render('practice1')
+})
+
+app.get('/practice1result', (req, res) => {
+    console.log('back', req.query)
+    res.send(req.query)
+})
+
+app.get("/practice2", (req, res) => {
+    res.render('practice2')
+})
+
+app.post('/practice2result', (req, res) => {
+    console.log(req.body.id)
+    console.log(req.body.pw)
+    if (req.body.id == id && req.body.pw == pw) {
+        res.send(true);
+    } else {
+        res.send(false);
+    }
+})
 
 // server start
 app.listen(PORT, () => {
