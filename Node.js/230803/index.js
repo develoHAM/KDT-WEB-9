a15230dc2bb166555d73e0b20270c9f1c7053a55
develoHAM <kdw980722@gmail.com>
2023-08-03 18:56:42 +0900

    동적 파일 업로드 실습

--- Node.js/230803/index.js
+++ Node.js/230803/index.js
@@ -36,6 +36,22 @@ const uploadDetail = multer({
     limits: {fileSize: 5 * 1024 * 1024},
 });
 
+const practice1multer = multer({
+    // storage: 저장할 공간에 대한 정보
+    // diskStorage: 파일을 디스크에 저장하기 위한 모든 제어 기능을 제공
+    storage: multer.diskStorage({
+        destination(req, file, done) {
+            done(null, 'uploads/');
+        },
+        filename(req, file, done) {
+            const ext = path.extname(file.originalname);
+            console.log('ext', ext);
+            done(null, req.body.id + ext);
+        }
+    }),
+    limits: {fileSize: 5 * 1024 * 1024},
+});
+
 // router
 app.get('/', (req, res) => {
     res.render('index');
@@ -62,6 +78,17 @@ app.post('/upload/fields', uploadDetail.fields([{name: 'userfile1'},{name: 'user
 // 동적 파일 
 app.post('/dynamicFile', uploadDetail.single('dynamic-file'), (req, res) => {
     console.log(req.file);
+    res.send(req.file);
+})
+
+//practice1
+app.get('/practice1', (req, res) => {
+    res.render('practice1');
+}) 
+app.post('/practice1server', practice1multer.single('profile-pic'), (req, res) => {
+    console.log('req.body', req.body)
+    console.log('req.body.id', req.body.id)
+    console.log('req.file', req.file)
     res.send(req.file);
 })
 
